5936fe682395eeb48ff3df2247e96dfcfb81966b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-04-05 13:13:09 +0200

    Dodanie przykładu wyświetlenia obrazka z pliku

--- src/Shopper.Domain/Product.cs
+++ src/Shopper.Domain/Product.cs
@@ -16,7 +16,7 @@ public class Product : BaseEntity
     //[Required, MaxLength(100), MinLength(3)]
     public string Name { get; set; }
     public string Description { get; set; }
-    public string Image { get; set; } = "https://via.placeholder.com/300x300";
+    public string Image { get; set; }
    // [Range(1, 1000)]
     public decimal Price { get; set; }
     public decimal PriceDiscounted { get; set; }
